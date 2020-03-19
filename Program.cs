360363a54c1a570aa9434a49dc04b8f847d63f49
Maher Husein <maherle03@gmail.com>
2020-03-19 17:19:18 -0400

    added 2d_array_ds solution

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace Hacker_Rank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is just a program that holds my solutions for hacker rank problems.");
+
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++) {
+                Console.WriteLine(i);
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+            
+            int result = TwoDArray.hourglassSum(arr);
+
+            Console.WriteLine(result);
         }
     }
 }
